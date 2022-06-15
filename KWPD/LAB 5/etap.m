906ae495c1d3315ea2f7d0161fa10e99b9fad15e
404Archi <rfieroch@gmail.com>
2022-06-15 18:11:25 +0200

    .

--- KWPD/LAB 5/etap.m
+++ KWPD/LAB 5/etap.m
@@ -1,4 +1,4 @@
-function [wynikA,wynikB,p_gr,d_gr] = etap(A, B)
+function [wynikA,wynikB,pgr,dgr] = etap(A, B)
 disp '==========================================='
 disp 'Rozwiazuje podgre dla:';
 A
@@ -7,72 +7,70 @@ k = min(A);
 Nashe = [];
 licznik = 0;
 for i=1:size(k')
-l = find(k(i) == A(:,i));
-for j=1:size(l);
-wiersz = B(l(j),:);
-m = min(wiersz);
-m = find(m == wiersz');
-m = m';
-for qq=1:length(m)
-qqq = m(qq);
-xxx = l(j);
-if(qqq == i)
-licznik = licznik+1;
-%disp 'rownowaga nasha dla ';
-wybor = [xxx qqq];
-wynik = [A(xxx, qqq) B(xxx, qqq)];
-Nashe = [Nashe; wybor];
-end
-end
-end
+    l = find(k(i) == A(:,i));
+    for j=1:size(l);
+        wiersz = B(l(j),:);
+        m = min(wiersz);
+        m = find(m == wiersz');
+        m = m';
+        for q=1:length(m)
+            qq = m(q);
+            xx = l(j);
+            if(qq == i)
+                licznik = licznik+1;
+                wybor = [xx qq];
+                wynik = [A(xx, qq) B(xx, qq)];
+                Nashe = [Nashe; wybor];
+            end
+        end
+    end
 end
 if licznik == 1
-wybor = wybor
-wynik = wynik
-p_gr = wybor(1);
-d_gr = wybor(2);
-wynikA = wynik(1);
-wynikB = wynik(2);
-return;
+    wybor = wybor
+    wynik = wynik
+    pgr = wybor(1);
+    dgr = wybor(2);
+    wynikA = wynik(1);
+    wynikB = wynik(2);
+    return;
 end
-Nashe
 for i=1:size(Nashe)
-k=0;
-strategia1 = Nashe(i,:);
-wynik1 = [A(strategia1(1), strategia1(2)) B(strategia1(1), strategia1(2))];
+    k=0;
+    strat1 = Nashe(i,:);
+    wynik1 = [A(strat1(1), strat1(2)) B(strat1(1), strat1(2))];
 
-for j=1:size(Nashe)
-if(i==j)
-continue;
-end
-strategia2 = Nashe(j,:);
-wynik2 = [A(strategia2(1), strategia2(2)) B(strategia2(1), strategia2(2))];
-if find((wynik1>wynik2) ~= 0)
-k=1;
-break;
-end
-end
-if k==0
-disp 'Strategia dopuszczalna '
-strategia1
-p_gr = strategia1(1);
-d_gr = strategia1(2);
-wynikA = A(p_gr, d_gr);
-wynikB = B(p_gr, d_gr);
-return;
-end
+    for j=1:size(Nashe)
+        if(i==j)
+            continue;
+        end
+        strat2 = Nashe(j,:);
+        wynik2 = [A(strat2(1), strat2(2)) B(strat2(1), strat2(2))];
+        if find((wynik1>wynik2) ~= 0)
+            k=1;
+            break;
+        end
+    end
+    if k==0
+        disp 'Strategia dopuszczalna '
+        strat1
+        pgr = strat1(1);
+        dgr = strat1(2);
+        wynikA = A(pgr, dgr);
+        wynikB = B(pgr, dgr);
+        return;
+    end
 end
 max1 = max(A');
 S1 = min(max1);
 i = find(max1 == S1);
-p_gr = i(1);
+pgr = i(1);
 max1 = max(B);
 S1 = min(max1);
 i = find(max1 == S1);
-d_gr = i(1);
-wynikA = A(p_gr, d_gr);
-wynikB = B(p_gr, d_gr);
+dgr = i(1);
+wynikA = A(pgr, dgr);
+wynikB = B(pgr, dgr);
 disp 'Strategia minimaxowa';
-strategia = [p_gr d_gr]
+strat = [pgr dgr]
 wynik = [wynikA wynikB]
 end
